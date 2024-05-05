0b47947c211b747bc1012c1d87a78bad2fe9cf8f
Dilshan Dananjaya <99714824+DilshanDevCraft@users.noreply.github.com>
2024-05-05 22:31:50 +0530

    app js

---
+++ frontend/src/Apps.js
@@ -0,0 +1,5 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navbar from "./Components/Navbar";
