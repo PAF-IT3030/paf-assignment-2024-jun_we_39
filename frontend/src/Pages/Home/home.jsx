9aad86d0e512fc06841059ba2b4653006badccdf
Dilshan Dananjaya <99714824+DilshanDevCraft@users.noreply.github.com>
2024-05-05 23:13:55 +0530

    Create home.jsx

---
+++ frontend/src/Pages/Home/home.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect } from "react";
+import Posts from "../../Components/Posts";
+import { useDispatch, useSelector } from "react-redux";
+import { getPosts } from "../../app/actions/post.actions";
+import NewUsersSuggest from "../../Components/NewUsersSuggest";
