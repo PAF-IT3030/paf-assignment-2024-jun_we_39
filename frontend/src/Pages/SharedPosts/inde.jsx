700f14f65a8ea2499e2d5429e429da1a7082b18b
Dilshan Dananjaya <99714824+DilshanDevCraft@users.noreply.github.com>
2024-05-05 23:58:14 +0530

    Update inde.jsx

--- frontend/src/Pages/SharedPosts/inde.jsx
+++ frontend/src/Pages/SharedPosts/inde.jsx
@@ -1,3 +1,5 @@
 import React, { useEffect } from "react";
 import Posts from "../../Components/Posts";
 import { useDispatch, useSelector } from "react-redux";
+import { getPostShareByUserId } from "../../app/actions/postshare.actions";
+import SharedPostsList from "../../Components/SharedPostsList";
