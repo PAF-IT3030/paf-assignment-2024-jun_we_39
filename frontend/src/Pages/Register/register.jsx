3f0fdf1ef9323f9183384adca20ef2fe6de6cdd0
Dilshan Dananjaya <99714824+DilshanDevCraft@users.noreply.github.com>
2024-05-05 23:49:37 +0530

    Create register.jsx

---
+++ frontend/src/Pages/Register/register.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { register } from "../../app/actions/user.actions";
+import "./Register.css"; // Import CSS file for styling
