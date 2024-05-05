ff04e0be91c27e2642c1d3838e9d44a9a90b9b79
Dilshan Dananjaya <99714824+DilshanDevCraft@users.noreply.github.com>
2024-05-06 00:00:23 +0530

    Update inde.jsx

--- frontend/src/Pages/SharedPosts/inde.jsx
+++ frontend/src/Pages/SharedPosts/inde.jsx
@@ -3,3 +3,9 @@ import Posts from "../../Components/Posts";
 import { useDispatch, useSelector } from "react-redux";
 import { getPostShareByUserId } from "../../app/actions/postshare.actions";
 import SharedPostsList from "../../Components/SharedPostsList";
+function SharedPosts() {
+    const dispatch = useDispatch();
+    const post = useSelector((state) => state.postshare);
+    const comment = useSelector((state) => state.comment);
+    const user = useSelector((state) => state.user);
+    useEffect(() => {
