5ef89cea0c91bf81a674eed1e3b12045b581c122
Dilshan Dananjaya <99714824+DilshanDevCraft@users.noreply.github.com>
2024-05-05 23:21:57 +0530

    Update login.jsx

--- frontend/src/Pages/Login/login.jsx
+++ frontend/src/Pages/Login/login.jsx
@@ -3,3 +3,10 @@ import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { login } from "../../app/actions/user.actions";
 import LoginImage from "../../assets/loginpic.png";
+
+
+
+function Login() {
+    const dispatch = useDispatch();
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
