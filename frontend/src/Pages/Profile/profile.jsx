0a7a51b4326fd816bd121e3ea38bf0349a6dc500
Dilshan Dananjaya <99714824+DilshanDevCraft@users.noreply.github.com>
2024-05-05 23:28:10 +0530

    Update profile.jsx

--- frontend/src/Pages/Profile/profile.jsx
+++ frontend/src/Pages/Profile/profile.jsx
@@ -2,3 +2,5 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getUser, updateUserById } from "../../app/actions/user.actions";
 import { deleteUserById } from "../../app/actions/user.actions";
+import storage from "../../util/firebaseConfig";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
