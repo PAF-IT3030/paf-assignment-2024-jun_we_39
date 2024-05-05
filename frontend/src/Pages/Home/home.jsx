e93b02cb9f2f9b8bba4c8e024ee728b66158278a
Dilshan Dananjaya <99714824+DilshanDevCraft@users.noreply.github.com>
2024-05-05 23:14:20 +0530

    Update home.jsx

--- frontend/src/Pages/Home/home.jsx
+++ frontend/src/Pages/Home/home.jsx
@@ -3,3 +3,9 @@ import Posts from "../../Components/Posts";
 import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "../../app/actions/post.actions";
 import NewUsersSuggest from "../../Components/NewUsersSuggest";
+
+
+function Home() {
+    const dispatch = useDispatch();
+    const post = useSelector((state) => state.post);
+  
