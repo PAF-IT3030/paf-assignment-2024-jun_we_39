387dc56202beff36a12ce08386f24df4b20868a4
Dilshan Dananjaya <99714824+DilshanDevCraft@users.noreply.github.com>
2024-05-05 23:27:48 +0530

    Create profile.jsx

---
+++ frontend/src/Pages/Profile/profile.jsx
@@ -0,0 +1,4 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUser, updateUserById } from "../../app/actions/user.actions";
+import { deleteUserById } from "../../app/actions/user.actions";
