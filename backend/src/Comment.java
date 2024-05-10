9b64dae390a3663b745622c2adc814e3a403ebff
it21258862 <99663609+IT21316722@users.noreply.github.com>
2024-05-10 23:58:30 +0530

    .
    ..

--- backend/src/Comment.java
+++ backend/src/Comment.java
@@ -1,4 +1,4 @@
+package com.paf.socialmedia.controller;
+
 import com.paf.socialmedia.document.Comment;
 import com.paf.socialmedia.service.CommentService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
