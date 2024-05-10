34cd49538b03900ab7918e79b9a0f7cd2ab5f163
Jayasankha Bandara <IT21261800@my.sliit.lk>
2024-05-10 22:13:16 +0530

    new comment

---
+++ backend/src/main/java/com/paf/socialmedia/document/commentt.java
@@ -0,0 +1,25 @@
+package com.paf.socialmedia.document;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "comments")
+public class Comment {
+    @Id
+    private String id;
+    private String text;
+    private String userId;
+    private String postId;
+    private Date createdAt;
+}
+    private Date updatedAt;
+}
