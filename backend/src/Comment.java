b5d3dcf596301d6edf10cb6088787ac15733ba41
it21258862 <99663609+IT21316722@users.noreply.github.com>
2024-05-10 22:01:19 +0530

    Comnt
    cmnt

--- backend/src/Comment.java
+++ backend/src/Comment.java
@@ -1,2 +1,4 @@
 import com.paf.socialmedia.document.Comment;
 import com.paf.socialmedia.service.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
