2c37dcf9ac681050b4c922e9906268385c1eb203
Dilshan Ranasinghe <99714600+IT21282140@users.noreply.github.com>
2024-05-10 22:31:27 +0530

    mealC2
    123

---
+++ backend/src/main/java/com/paf/socialmedia/controller/MealController.java
@@ -0,0 +1,9 @@
+package com.paf.socialmedia.controller;
+
+import com.example.pafbackend.models.MealPlan;
+import com.example.pafbackend.repositories.MealPlanRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MealController {
+    
+}
