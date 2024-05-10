ca4f71f3f93c130467ffe8e7503d25ce9b451bde
Dilshan Ranasinghe <99714600+IT21282140@users.noreply.github.com>
2024-05-10 22:32:38 +0530

    mealC3
    123

--- backend/src/main/java/com/paf/socialmedia/controller/MealController.java
+++ backend/src/main/java/com/paf/socialmedia/controller/MealController.java
@@ -3,6 +3,9 @@ package com.paf.socialmedia.controller;
 import com.example.pafbackend.models.MealPlan;
 import com.example.pafbackend.repositories.MealPlanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 public class MealController {
     
