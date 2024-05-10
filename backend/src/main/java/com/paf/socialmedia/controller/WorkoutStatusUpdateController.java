5597018f77772dd508cfee685374b8cc84a92bec
Jayasankha Bandara <IT21261800@my.sliit.lk>
2024-05-10 22:51:11 +0530

    get mapping

--- backend/src/main/java/com/paf/socialmedia/controller/WorkoutStatusUpdateController.java
+++ backend/src/main/java/com/paf/socialmedia/controller/WorkoutStatusUpdateController.java
@@ -1,10 +1,32 @@
 package com.example.pafbackend.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.pafbackend.models.WorkoutStatusUpdate;
+import com.example.pafbackend.repositories.WorkoutStatusUpdateRepository;
 
 @RestController
 @RequestMapping("/api/workoutStatusUpdates")
 public class WorkoutStatusUpdateController {
 
     private final WorkoutStatusUpdateRepository workoutStatusUpdateRepository;
+
+    @Autowired
+    public WorkoutStatusUpdateController(WorkoutStatusUpdateRepository workoutStatusUpdateRepository) {
+        this.workoutStatusUpdateRepository = workoutStatusUpdateRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<WorkoutStatusUpdate>> getUpdatesByUserId() {
+        List<WorkoutStatusUpdate> updates = workoutStatusUpdateRepository.findAll();
+        return new ResponseEntity<>(updates, HttpStatus.OK);
+    }
+
+   
