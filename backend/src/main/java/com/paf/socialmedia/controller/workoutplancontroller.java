49ed75830a49bff55fd772736c4fdb7e743dd243
Jayasankha Bandara <IT21261800@my.sliit.lk>
2024-05-10 23:06:33 +0530

    post

--- backend/src/main/java/com/paf/socialmedia/controller/workoutplancontroller.java
+++ backend/src/main/java/com/paf/socialmedia/controller/workoutplancontroller.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,4 +37,10 @@ public class WorkoutPlanController {
         return new ResponseEntity<>(workoutPlans, HttpStatus.OK);
     }
 
-   
+    @PostMapping
+    public ResponseEntity<WorkoutPlan> createWorkoutPlan(@RequestBody WorkoutPlan workoutPlan) {
+        WorkoutPlan savedWorkoutPlan = workoutPlanRepository.save(workoutPlan);
+        return new ResponseEntity<>(savedWorkoutPlan, HttpStatus.CREATED);
+    }
+
+    
