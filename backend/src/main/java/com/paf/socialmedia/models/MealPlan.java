a4ad7014b16b1e4f0ef126622b4aef3be46dc5b2
Dilshan Ranasinghe <99714600+IT21282140@users.noreply.github.com>
2024-05-10 22:47:40 +0530

    mealM3

--- backend/src/main/java/com/paf/socialmedia/models/MealPlan.java
+++ backend/src/main/java/com/paf/socialmedia/models/MealPlan.java
@@ -2,6 +2,8 @@ package com.paf.socialmedia.models;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 public class MealPlan {
     
