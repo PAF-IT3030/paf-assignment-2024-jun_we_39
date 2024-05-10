d24e90fc82a153173f2a346298feaedde8249c13
it21258862 <99663609+IT21316722@users.noreply.github.com>
2024-05-11 02:26:23 +0530

    Update CommentController.java

--- backend/src/CommentController.java
+++ backend/src/CommentController.java
@@ -36,7 +36,7 @@ public class CommentController {
     }
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteCommentById(@PathVariable String id){
-        return commentService.deleteCommentById(id);
+        retur commentService.deleteCommentById(id);
     }
 }
 
