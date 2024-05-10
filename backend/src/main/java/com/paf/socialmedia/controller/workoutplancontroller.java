7a482d48104210266912eb1fbefdb1bdb5bb754d
Jayasankha Bandara <IT21261800@my.sliit.lk>
2024-05-10 23:02:25 +0530

    getting map

--- backend/src/main/java/com/paf/socialmedia/controller/workoutplancontroller.java
+++ backend/src/main/java/com/paf/socialmedia/controller/workoutplancontroller.java
@@ -1,9 +1,15 @@
 package com.example.pafbackend.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.pafbackend.models.WorkoutPlan;
 import com.example.pafbackend.repositories.WorkoutPlanRepository;
 @RestController
 @RequestMapping("/api/workoutPlans")
@@ -16,4 +22,9 @@ public class WorkoutPlanController {
         this.workoutPlanRepository = workoutPlanRepository;
     }
 
-    
+    @GetMapping
+    public ResponseEntity<List<WorkoutPlan>> getWorkoutPlans() {
+        List<WorkoutPlan> workoutPlans = workoutPlanRepository.findAll();
+        return new ResponseEntity<>(workoutPlans, HttpStatus.OK);
+    }
+
