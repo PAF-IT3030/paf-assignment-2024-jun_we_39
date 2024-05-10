72ea72d0370ba692dd20d9436febf35d725d5afa
Dilshan Ranasinghe <99714600+IT21282140@users.noreply.github.com>
2024-05-10 23:57:38 +0530

    commnetS02

---
+++ backend/src/main/java/com/paf/socialmedia/service/CommentService02.java
@@ -0,0 +1,9 @@
+package com.paf.socialmedia.service;
+
+import com.paf.socialmedia.document.Notification;
+import com.paf.socialmedia.repository.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class CommentService02 {
+    
+}
