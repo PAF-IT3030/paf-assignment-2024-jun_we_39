13938bf47f093dc35543a70e0d4232edf3a18697
Jayasankha Bandara <IT21261800@my.sliit.lk>
2024-05-10 22:51:52 +0530

    post

--- backend/src/main/java/com/paf/socialmedia/controller/WorkoutStatusUpdateController.java
+++ backend/src/main/java/com/paf/socialmedia/controller/WorkoutStatusUpdateController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +32,17 @@ public class WorkoutStatusUpdateController {
         return new ResponseEntity<>(updates, HttpStatus.OK);
     }
 
-   
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<WorkoutStatusUpdate>> getUpdatesByUserId(@PathVariable String userId) {
+        List<WorkoutStatusUpdate> updates = workoutStatusUpdateRepository.findByUserId(userId);
+        return new ResponseEntity<>(updates, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<WorkoutStatusUpdate> createUpdate(@RequestBody WorkoutStatusUpdate update) {
+        WorkoutStatusUpdate savedUpdate = workoutStatusUpdateRepository.save(update);
+        return new ResponseEntity<>(savedUpdate, HttpStatus.CREATED);
+    }
+
+    
+
