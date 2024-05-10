fcbd21fade8eebec103af10f21d1e86ddeb5b8e3
Dilshan Ranasinghe <99714600+IT21282140@users.noreply.github.com>
2024-05-10 22:52:10 +0530

    mealR01

--- backend/src/main/java/com/paf/socialmedia/repository/MealPlanRepository.java
+++ backend/src/main/java/com/paf/socialmedia/repository/MealPlanRepository.java
@@ -1,5 +1,8 @@
 package com.paf.socialmedia.repository;
 
+import com.example.pafbackend.models.MealPlan;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
 public class MealPlanRepository {
     
 }
