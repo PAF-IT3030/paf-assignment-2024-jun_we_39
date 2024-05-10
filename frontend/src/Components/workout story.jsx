0524c8f0452514d5213a007a49b338fa429c3e11
Jayasankha Bandara <IT21261800@my.sliit.lk>
2024-05-11 00:21:45 +0530

    update

--- frontend/src/Components/workout story.jsx
+++ frontend/src/Components/workout story.jsx
@@ -1,7 +1,8 @@
-import { Form } from "antd";
+import { Form, message } from "antd";
 import { useEffect, useState } from "react";
 import { useSnapshot } from "valtio";
 import UploadFileService from "../../Services/UploadFileService";
+import WorkoutStoryService from "../../Services/WorkoutStoryService";
 import state from "../../Utils/Store";
 
 const uploadService = new UploadFileService();
@@ -28,3 +29,47 @@ const WorkoutStory = () => {
     description: workoutStory?.description || "",
   });
 
+  const handleUpdate = async () => {
+    try {
+      setLoading(true);
+      await WorkoutStoryService.updateWorkoutStory(
+        snap.selectedWorkoutStory.id,
+        updatedStory
+      );
+      state.storyCards = await WorkoutStoryService.getAllWorkoutStories();
+      state.workoutStoryOpen = false;
+      message.success("Successfully updated");
+      form.resetFields();
+    } catch (error) {
+      message.success("Error while deleting story");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleDelete = async () => {
+    try {
+      setDeleteLoading(true);
+      await WorkoutStoryService.deleteWorkoutStory(
+        snap.selectedWorkoutStory.id
+      );
+      state.storyCards = await WorkoutStoryService.getAllWorkoutStories();
+      state.workoutStoryOpen = false;
+      message.success("Workout story deleted successfully");
+    } catch (error) {
+      message.error("Failed to delete story");
+    } finally {
+      setDeleteLoading(false);
+    }
+  };
+
+  const handleCancel = () => {
+    form.resetFields(); // Reset form fields to initial values
+    setUpdatedStory({
+      title: workoutStory?.title || "",
+      image: workoutStory?.image || "",
+      description: workoutStory?.description || "",
+    });
+  };
+
+  
