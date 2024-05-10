5b0f99bbef8f08ed88d09378c3fe76247f45781b
Dilshan Ranasinghe <99714600+IT21282140@users.noreply.github.com>
2024-05-10 23:58:15 +0530

    commentS03

--- backend/src/main/java/com/paf/socialmedia/service/CommentService02.java
+++ backend/src/main/java/com/paf/socialmedia/service/CommentService02.java
@@ -3,6 +3,9 @@ package com.paf.socialmedia.service;
 import com.paf.socialmedia.document.Notification;
 import com.paf.socialmedia.repository.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 public class CommentService02 {
     
