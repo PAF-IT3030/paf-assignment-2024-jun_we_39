fe2f9652c3bcbdc3ccfe3cb59f1b275a2be1e8b6
Dilshan Ranasinghe <99714600+IT21282140@users.noreply.github.com>
2024-05-10 22:57:42 +0530

    upmealC1

--- backend/src/main/java/com/paf/socialmedia/controller/MealController.java
+++ backend/src/main/java/com/paf/socialmedia/controller/MealController.java
@@ -1,6 +1,6 @@
 package com.paf.socialmedia.controller;
 
-import com.example.pafbackend.models.MealPlan;
+import com.paf.socialmedia.models.MealPlan;
 import com.example.pafbackend.repositories.MealPlanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
