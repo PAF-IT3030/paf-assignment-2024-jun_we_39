335312c496cbcb295a75b602fa38c9ccb97cfe52
it21258862 <99663609+IT21316722@users.noreply.github.com>
2024-05-11 01:17:12 +0530

    import
    import

--- backend/src/Comment.java
+++ backend/src/Comment.java
@@ -3,3 +3,4 @@ package com.paf.socialmedia.controller;
 import com.paf.socialmedia.document.Comment;
 import com.paf.socialmedia.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
