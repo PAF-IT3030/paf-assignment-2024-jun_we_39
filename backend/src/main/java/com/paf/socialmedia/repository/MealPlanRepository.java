b7097e4c487dec7f896246294084d797abff85aa
Dilshan Ranasinghe <99714600+IT21282140@users.noreply.github.com>
2024-05-10 22:53:58 +0530

    mealR7

--- backend/src/main/java/com/paf/socialmedia/repository/MealPlanRepository.java
+++ backend/src/main/java/com/paf/socialmedia/repository/MealPlanRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 @Repository
 
-public class MealPlanRepository {
-    
+public class MealPlanRepository extends MongoRepository<MealPlan, String> {
+    List<MealPlan> findByUserId(String userId);
+
 }
