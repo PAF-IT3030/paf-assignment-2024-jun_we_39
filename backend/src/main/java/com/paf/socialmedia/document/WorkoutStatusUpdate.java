6273606f898919b4033ddcae760006f02515ba1c
Jayasankha Bandara <IT21261800@my.sliit.lk>
2024-05-11 00:08:22 +0530

    new

---
+++ backend/src/main/java/com/paf/socialmedia/document/WorkoutStatusUpdate.java
@@ -0,0 +1,18 @@
+package com.example.pafbackend.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Document(collection = "workoutStatusUpdates")
+@Getter
+@Setter
+public class WorkoutStatusUpdate {
+    @Id
+    private String id;
+    
+
+
+}
