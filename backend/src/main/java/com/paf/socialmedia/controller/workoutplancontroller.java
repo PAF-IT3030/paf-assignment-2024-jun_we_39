bb84f2c9c0b46140aaeca8cea1554bae8dda4911
Jayasankha Bandara <IT21261800@my.sliit.lk>
2024-05-10 23:03:45 +0530

    Get

--- backend/src/main/java/com/paf/socialmedia/controller/workoutplancontroller.java
+++ backend/src/main/java/com/paf/socialmedia/controller/workoutplancontroller.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,3 +29,10 @@ public class WorkoutPlanController {
         return new ResponseEntity<>(workoutPlans, HttpStatus.OK);
     }
 
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<WorkoutPlan>> getWorkoutPlansByUserId(@PathVariable String userId) {
+        List<WorkoutPlan> workoutPlans = workoutPlanRepository.findByUserId(userId);
+        return new ResponseEntity<>(workoutPlans, HttpStatus.OK);
+    }
+
+   
